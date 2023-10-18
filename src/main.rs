3c694982765cef198e55751c950efae39474e164
Edison Moreland <edison@devdude.net>
2023-10-17 18:54:50 -0700

    first window

--- src/main.rs
+++ src/main.rs
@@ -7,76 +7,100 @@ use crate::relaxation::RelaxationSystem;
 use crate::sampling::sample;
 use crate::surfaces::{ellipsoid, gradient, smooth_union, sphere, translate, union};
 use nalgebra::{vector, Vector3};
-use raylib::prelude::*;
 
-fn surface_at(t: f32) -> impl Fn(Vector3<f32>) -> f32 {
-    smooth_union(
-        sphere(10.0),
-        union(
-            translate(
-                vector![(t).sin() * 10.0, 0.0, 0.0],
-                ellipsoid(10.0, 5.0, 5.0),
-            ),
-            translate(
-                vector![0.0, 0.0, (t).cos() * 10.0],
-                ellipsoid(5.0, 5.0, 10.0),
-            ),
-        ),
-        0.5,
-    )
-}
+// fn surface_at(t: f32) -> impl Fn(Vector3<f32>) -> f32 {
+//     smooth_union(
+//         sphere(10.0),
+//         union(
+//             translate(
+//                 vector![(t).sin() * 10.0, 0.0, 0.0],
+//                 ellipsoid(10.0, 5.0, 5.0),
+//             ),
+//             translate(
+//                 vector![0.0, 0.0, (t).cos() * 10.0],
+//                 ellipsoid(5.0, 5.0, 10.0),
+//             ),
+//         ),
+//         0.5,
+//     )
+// }
+
+// fn main() {
+//     let (mut rl, thread) = raylib::init().size(640, 480).title("Hello, World").build();
+//
+//     let camera = Camera3D::perspective(
+//         rvec3(25.0, 25.0, 25.0),
+//         rvec3(0.0, 0.0, 0.0),
+//         math::Vector3::up(),
+//         40.0,
+//     );
+//
+//     let seed = vector![0.0, 10.0, 0.0];
+//     let sample_radius = 0.5;
+//
+//     let mut t = 0.0;
+//     let surface = surface_at(t);
+//     let points = sample(surface, seed, sample_radius);
+//
+//     let mut particles = RelaxationSystem::new(points, sample_radius);
+//
+//     while !rl.window_should_close() {
+//         let mut d = rl.begin_drawing(&thread);
+//
+//         d.clear_background(Color::WHITE);
+//
+//         t += 0.03;
+//         let surface = surface_at(t);
+//
+//         {
+//             let mut d3d = d.begin_mode3D(camera);
+//             for (point, radius) in particles.positions() {
+//                 let normal = gradient(&surface, point).normalize();
+//
+//                 let point_color = Color::color_from_normalized(Vector4::new(
+//                     normal.x.abs(),
+//                     normal.y.abs(),
+//                     normal.z.abs(),
+//                     1.0,
+//                 ));
+//
+//                 let true_position = point - normal.scale(radius * 2.0);
+//
+//                 d3d.draw_sphere(
+//                     rvec3(true_position.x, true_position.y, true_position.z),
+//                     radius * 2.0,
+//                     point_color,
+//                 )
+//             }
+//         }
+//
+//         particles.update(sample_radius, &surface);
+//
+//         d.draw_fps(0, 0);
+//     }
+// }
+use winit::event::StartCause;
+use winit::event_loop::ControlFlow;
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::EventLoop,
+    window::WindowBuilder,
+};
 
 fn main() {
-    let (mut rl, thread) = raylib::init().size(640, 480).title("Hello, World").build();
+    let event_loop = EventLoop::new().unwrap();
+    let mut window = None;
 
-    let camera = Camera3D::perspective(
-        rvec3(25.0, 25.0, 25.0),
-        rvec3(0.0, 0.0, 0.0),
-        math::Vector3::up(),
-        40.0,
-    );
-
-    let seed = vector![0.0, 10.0, 0.0];
-    let sample_radius = 0.5;
-
-    let mut t = 0.0;
-    let surface = surface_at(t);
-    let points = sample(surface, seed, sample_radius);
-
-    let mut particles = RelaxationSystem::new(points, sample_radius);
-
-    while !rl.window_should_close() {
-        let mut d = rl.begin_drawing(&thread);
-
-        d.clear_background(Color::WHITE);
-
-        t += 0.03;
-        let surface = surface_at(t);
-
-        {
-            let mut d3d = d.begin_mode3D(camera);
-            for (point, radius) in particles.positions() {
-                let normal = gradient(&surface, point).normalize();
-
-                let point_color = Color::color_from_normalized(Vector4::new(
-                    normal.x.abs(),
-                    normal.y.abs(),
-                    normal.z.abs(),
-                    1.0,
-                ));
-
-                let true_position = point - normal.scale(radius * 2.0);
-
-                d3d.draw_sphere(
-                    rvec3(true_position.x, true_position.y, true_position.z),
-                    radius * 2.0,
-                    point_color,
-                )
+    event_loop
+        .run(move |event, event_loop, cf| match event {
+            Event::NewEvents(StartCause::Init) => {
+                window = Some(WindowBuilder::new().build(&event_loop).unwrap())
             }
-        }
-
-        particles.update(sample_radius, &surface);
-
-        d.draw_fps(0, 0);
-    }
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                window_id,
+            } if window_id == window.as_ref().unwrap().id() => *cf = ControlFlow::ExitWithCode(0),
+            _ => (),
+        })
+        .unwrap();
 }
