d10123874664b115394332f414e9e0b8805ec988
Edison Moreland <edison@devdude.net>
2023-10-18 19:23:57 -0700

    Cleanup

--- build.rs
+++ build.rs
@@ -1,15 +1,12 @@
 use std::env;
 use std::path::PathBuf;
-use std::process::Command;
+use std::process::{Command, Output};
 
 fn main() {
-    compile_shader(&PathBuf::from("src/shader.metal"));
+    compile_shader(&PathBuf::from("src/renderer/shader.metal"));
 }
 
-// xcrun -sdk macosx metal -c shaders.metal -o shaders.air
-// xcrun -sdk macosx metallib shaders.air -o shaders.metallib
 fn compile_shader(shader_source: &PathBuf) {
-    // TODO: Rewrite all of this
     println!("cargo:rerun-if-changed={}", shader_source.to_str().unwrap());
 
     let shader_name = shader_source
@@ -27,44 +24,34 @@ fn compile_shader(shader_source: &PathBuf) {
     let air_path = intermediate_dir.join(format!("{}.air", shader_name));
     let metallib_path = out_dir.join(format!("{}.metallib", shader_name));
 
-    let output = Command::new("xcrun")
-        .arg("-sdk")
-        .arg("macosx")
-        .arg("metal")
-        .args(&["-gline-tables-only"])
-        .args(&["-frecord-sources"])
-        .args(&["-c", shader_source.to_str().unwrap()])
-        .args(&["-o", air_path.to_str().unwrap()])
-        .spawn()
-        .unwrap()
-        .wait_with_output()
-        .unwrap();
-    if !output.status.success() {
-        panic!(
-            r#"
-            air
-stdout: {}
-stderr: {}
-"#,
-            String::from_utf8(output.stdout).unwrap(),
-            String::from_utf8(output.stderr).unwrap()
-        );
-    }
+    panic_if_failed(Command::new("xcrun").args(&[
+        "-sdk",
+        "macosx",
+        "metal",
+        "-gline-tables-only",
+        "-frecord-sources",
+        "-c",
+        shader_source.to_str().unwrap(),
+        "-o",
+        air_path.to_str().unwrap(),
+    ]));
 
-    let output = Command::new("xcrun")
-        .arg("-sdk")
-        .arg("macosx")
-        .arg("metallib")
-        .arg(&air_path)
-        .args(&["-o", metallib_path.to_str().unwrap()])
-        .spawn()
-        .unwrap()
-        .wait_with_output()
-        .unwrap();
+    panic_if_failed(Command::new("xcrun").args(&[
+        "-sdk",
+        "macosx",
+        "metallib",
+        air_path.to_str().unwrap(),
+        "-o",
+        metallib_path.to_str().unwrap(),
+    ]));
+}
+
+fn panic_if_failed(command: &mut Command) {
+    let output = command.spawn().unwrap().wait_with_output().unwrap();
+
     if !output.status.success() {
         panic!(
             r#"
-            metallib
 stdout: {}
 stderr: {}
 "#,
